872ca7b476ebb2ea01622d93cf07cdd55e144c55
SwitchBoard <xswitchboardx@gmail.com>
2015-10-06 12:03:37 -0400

    2D Dark Souls Commit1
    Adding to Repo

---
+++ 2D Dark Souls/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    float m_movementSpeed;
+    int m_health;
+    float m_h;
+    void Move()
+    {
+        
+        m_h = Input.GetAxis("Horizontal");
+        if (m_h > 0.0f)
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x + m_movementSpeed, gameObject.transform.position.y);
+        else if (m_h < 0.0f)
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x - m_movementSpeed, gameObject.transform.position.y);
+    }
+
+	void Start () 
+    {
+        m_movementSpeed = 0.02f;
+        m_health = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Move();
+	}
+}
